db5a77f1490e674f4b78bf59ff7c7d2c5c8578f0
Maxime Betton <m.betton@betclicgroup.com>
2021-04-21 18:51:28 +0200

    clean code

--- WeatherApp/Views/AddView.swift
+++ WeatherApp/Views/AddView.swift
@@ -21,12 +21,10 @@ struct AddView: View {
         self.viewModel = viewModel
     }
     
-    
-    
     func verify() -> Bool {
         var result: Bool = false
         for city in cities {
-            if viewModel.city == city.city {
+            if viewModel.city == city.city || viewModel.dataSource.isEmpty{
                 result = false
                 break
             } else {
@@ -38,87 +36,59 @@ struct AddView: View {
     
     var body: some View {
         GeometryReader { geo in
-            
-            VStack{
-                AnimatedImage(url: URL(string:"https://i.pinimg.com/originals/cd/25/45/cd25452763ef54225089858996d1b7ff.gif"))
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .edgesIgnoringSafeArea(.all)
-                    .frame(width: geo.size.width, height: geo.size.height)
-                    .overlay(
-                        Form {
-                            //searchField
-                            Section(header: Text("Ville")) {
-                                TextField("Paris", text: self.$viewModel.city)
+            NavigationView {
+                VStack{
+                    List {
+                        Section(header: Text("Ville")) {
+                            TextField("Paris", text: self.$viewModel.city)
+                        }
+                        
+                        Button(action: {
+                            self.viewModel.addCity(context: context)
+                        }) {
+                            Text("Ajouter la ville")
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        }.disabled(!self.verify())
+                        if viewModel.dataSource.isEmpty {
+                            Section {
+                                Text("No results")
+                                    .foregroundColor(.gray)
                             }
-                            
-                            Button(action: {
-                                self.viewModel.addCity(context: context)
-                            }) {
-                                Text("Ajouter la ville")
-                                    .frame(maxWidth: .infinity, alignment: .center)
+                        } else {
+                            VStack{
+                                NavigationLink(destination: viewModel.currentWeatherView) {
+                                    VStack(alignment: .leading) {
+                                        Spacer()
+                                        Text("Weather today")
+                                            .bold()
+                                            .font(.system(size: 25))
+                                            .foregroundColor(.blue)
+                                    }
+                                }
+                                Spacer()
                             }
-                            
-                            if viewModel.dataSource.isEmpty {
-                                emptySection
-                            } else {
-                                cityHourlyWeatherSection
-                                forecastSection
+                            VStack{
+                                Spacer()
+                                ForEach(viewModel.dataSource ) { dailyWeatherVM in
+                                    DailyWeatherRowView(viewModel: dailyWeatherVM)
+                                }
                             }
-                            
-                        })
-                    .navigationBarTitle("Ajouter")
-            }
-        }
-        .onAppear(perform: {
-            viewModel.city = ""
-        })
-    }
-}
-
-//struct AddView_Previews: PreviewProvider {
-//static var previews: some View {
-//    AddView()
-//  }
-//}
-//private extension AddView {
-//    var searchField: some View {
-//      HStack(alignment: .center) {
-//        TextField("e.g. Cupertino", text: $viewModel.city)
-//      }
-//    }
-//}
-
-private extension AddView {
-    var searchField: some View {
-        HStack(alignment: .center) {
-            TextField("e.g. Cupertino", text: $viewModel.city)
-        }
-    }
-    
-    var forecastSection: some View {
-        Section {
-            ForEach(viewModel.dataSource, content: DailyWeatherRowView.init(viewModel:))
-        }
-    }
-    
-    var cityHourlyWeatherSection: some View {
-        Section {
-            NavigationLink(destination: viewModel.currentWeatherView) {
-                VStack(alignment: .leading) {
-                    Text(viewModel.city)
-                    Text("Weather today")
-                        .font(.caption)
-                        .foregroundColor(.gray)
+                        }
+                        
+                    }
+                    .opacity(0.7)
+                    .listStyle(InsetGroupedListStyle())
+                    .background(AnimatedImage(url: URL(string:"https://i.pinimg.com/originals/cd/25/45/cd25452763ef54225089858996d1b7ff.gif"))
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .edgesIgnoringSafeArea(.all)
+                                    .frame(width: geo.size.width, height: geo.size.height))
                 }
+                .navigationBarTitle("Ajouter")
             }
-        }
-    }
-    
-    var emptySection: some View {
-        Section {
-            Text("No results")
-                .foregroundColor(.gray)
+            .onAppear(perform: {
+                viewModel.city = ""
+            })
         }
     }
 }
